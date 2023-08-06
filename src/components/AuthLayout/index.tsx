c542d788f8877c2f43f1637d7020ba2d354120fb
hwinkr <dnddl8280@naver.com>
2023-08-07 00:56:22 +0900

    Feat: 로그인 상태 확인하는 HOC 컴포넌트 구현

--- src/components/AuthLayout/index.tsx
+++ src/components/AuthLayout/index.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import useRouter from '../../hooks/useRouter';
+import { Navigate } from 'react-router-dom';
+import { getTokenFromLocalStorage } from '../../apis/token';
 
 interface AuthLayoutProps {
   children: React.ReactNode;
 }
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
-  console.log(1);
+  const { current } = useRouter();
+
+  if (current === '/todo' && !getTokenFromLocalStorage()) {
+    alert('로그인 토큰이 발급되지 않았어요! 로그인 후 이용해주세요');
+    return <Navigate to="/signin" />;
+  }
+  if (
+    (current === '/signin' || current === '/signup') &&
+    getTokenFromLocalStorage()
+  ) {
+    alert('현재 로그인 토큰이 발급되어 있어요! 투두 리스트 페이지로 이동해요.');
+    return <Navigate to="/todo" />;
+  }
+
   return <>{children}</>;
 };
 

e0605002f30e5b3476be353607793f26ff789d18
hwinkr <dnddl8280@naver.com>
2023-08-07 00:58:52 +0900

    Config: 요청 헤더에 access_token 추가하도록 설정

--- src/apis/http.ts
+++ src/apis/http.ts
@@ -1,8 +1,18 @@
 import axios, { AxiosInstance } from 'axios';
 import env from '../config';
+import { getTokenFromLocalStorage } from './token';
 
 const http: AxiosInstance = axios.create({
   baseURL: env.SERVER_URL,
+  headers: { 'Content-Type': 'application/json' },
+});
+
+http.interceptors.request.use((config) => {
+  const accessToken = getTokenFromLocalStorage();
+  if (accessToken) {
+    config.headers['Authorization'] = `Bearer ${accessToken}`;
+  }
+  return config;
 });
 
 export default http;

23a4e5ed31e10b848fb197f0541542b566fc454b
hwinkr <dnddl8280@naver.com>
2023-08-11 13:35:04 +0900

    Feat: todo 페이지 구현

--- src/pages/Todo/index.tsx
+++ src/pages/Todo/index.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect, useMemo } from 'react';
+import { CreateTodo, TodoList } from '../../components/Todo';
+import useTodo from '../../hooks/useTodo';
 
 const Todo = () => {
-  return <div>Todo</div>;
+  const { todos, getTodoList } = useTodo();
+  useEffect(() => {
+    getTodoList();
+  }, []);
+
+  const props = useMemo(
+    () => ({
+      todos,
+      getTodoList,
+    }),
+    [todos, getTodoList],
+  );
+
+  return (
+    <>
+      <CreateTodo getTodoList={props.getTodoList} />
+      <TodoList todos={props.todos} />
+    </>
+  );
 };
 
 export default Todo;

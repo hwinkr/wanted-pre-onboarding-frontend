6b5b977beba3db0514c3db09342887273d5ec1d6
hwinkr <dnddl8280@naver.com>
2023-08-11 13:34:19 +0900

    홈페이지 구현

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import { styled } from 'styled-components';
+import { useRouter } from '../../hooks';
+import Button from '../../components/Button';
 
 const Home = () => {
-  return <div>Home</div>;
+  const { routerTo } = useRouter();
+  return (
+    <Container>
+      <Title>Todo List</Title>
+      <ButtonContainer>
+        <Button onClick={() => routerTo('/signin')}>로그인</Button>
+        <Button onClick={() => routerTo('/signup')}>회원가입</Button>
+      </ButtonContainer>
+    </Container>
+  );
 };
 
 export default Home;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Title = styled.span`
+  font-size: 3em;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  width: 30%;
+  justify-content: space-between;
+
+  margin-top: 30px;
+`;

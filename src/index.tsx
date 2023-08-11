4db577781a12575984f3051c1ce97548a05bf6d3
hwinkr <dnddl8280@naver.com>
2023-08-11 13:46:32 +0900

    Chore: GlobalStyleProvider로 이름 변경

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import GlobalStyleProvider from './styles/ThemeProvider';
+import GlobalStyleProvider from './styles/GlobalStyleProvider';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,

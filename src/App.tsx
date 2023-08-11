16e6e9d26cccdab27236f1eafb0a14f8840056cf
hwinkr <dnddl8280@naver.com>
2023-08-11 12:29:54 +0900

    Feat: App Router 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
-import { RouterProvider } from 'react-router-dom';
-import { routers } from './router';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Todo from './pages/Todo';
+import AuthLayout from './components/AuthLayout';
+import TodoProvider from './components/Provider/TodoProvider';
 
 const App = () => {
-  return <RouterProvider router={routers} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route element={<AuthLayout />}>
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route element={<TodoProvider />}>
+            <Route path="/todo" element={<Todo />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
